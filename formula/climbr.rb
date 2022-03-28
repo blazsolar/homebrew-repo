6994b27da853c5023b9c03a1c82a90400719f8d3
Blaz Solar <me@blaz.solar>
2022-03-28 11:59:45 +0200

    Update

--- formula/climbr.rb
+++ formula/climbr.rb
@@ -3,7 +3,7 @@ require "download_strategy"
 class Climbr < Formula
   desc "CLI interface for climbr app"
   homepage "https://github.com/blazsolar/climbr-cli"
-  url "https://github.com/blazsolar/climbr-cli/releases/download/v0.1/climbr.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/blazsolar/climbr-cli/releases/download/v0.1/climbr.tar.gz", :using => GitHubGitDownloadStrategy
   sha256 "<SHA256>"
   head "https://github.com/blazsolar/climbr-cli.git"
 
