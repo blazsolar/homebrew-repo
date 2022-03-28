bc6f6e16e05d6fd59f9ae3625434fb4411496ed3
Blaz Solar <me@blaz.solar>
2022-03-28 12:24:57 +0200

    Downalod strategy

--- formula/climbr.rb
+++ formula/climbr.rb
@@ -1,9 +1,9 @@
-require "download_strategy"
+require_relative "lib/custom_download_strategy"
 
 class Climbr < Formula
   desc "CLI interface for climbr app"
   homepage "https://github.com/blazsolar/climbr-cli"
-  url "git@github.com:blazsolar/climbr-cli.git", :using => GitHubGitDownloadStrategy
+  url "https://github.com/blazsolar/climbr-cli/releases/download/v0.1/climbr-0.1-darwind_m1.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "<SHA256>"
   head "https://github.com/blazsolar/climbr-cli.git"
 
