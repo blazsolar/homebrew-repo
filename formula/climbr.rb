1eb91d03303244c55ff00025b5958a87490f27b4
Blaz Solar <me@blaz.solar>
2022-03-28 14:11:30 +0200

    Update to 0.3

--- formula/climbr.rb
+++ formula/climbr.rb
@@ -3,15 +3,29 @@ require_relative "../lib/custom_download_strategy"
 class Climbr < Formula
   desc "CLI interface for climbr app"
   homepage "https://github.com/blazsolar/climbr-cli"
-  url "https://github.com/blazsolar/climbr-cli/releases/download/v0.2/climbr.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "dc081dc727f68a648486c7d3cf341ecc7d1c8aed1c958d0049fd64a2cc05e4b7"
   head "https://github.com/blazsolar/climbr-cli.git"
+  version "0.3"
 
-  def install
-    bin.install "climbr"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/blazsolar/climbr-cli/releases/download/v0.3/climbr.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "d035ef3ef8b8a55abaaa19580d3348e453ba7bd46f22d19dfb52356090293942"
+
+      def install
+        bin.install "climbr"
+      end
+    end
+#    if Hardware::CPU.intel?
+#      url "https://github.com/myorg/mytool/releases/download/v1.1.5/mytool_1.1.5_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+#      sha256 "qwerty987..."
+#
+#      def install
+#        bin.install "mytool"
+#      end
+#    end
   end
   
   test do
-    assert_match "climbr 0.1", shell_output("#{bin}/climbr --version", 2)
+    assert_match "climbr 0.3", shell_output("#{bin}/climbr --version", 2)
   end
 end
