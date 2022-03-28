84c08819ee0cde94b866b46b014a81ab6f30d59e
Blaz Solar <me@blaz.solar>
2022-03-28 12:27:44 +0200

    update SHA

--- formula/climbr.rb
+++ formula/climbr.rb
@@ -4,7 +4,7 @@ class Climbr < Formula
   desc "CLI interface for climbr app"
   homepage "https://github.com/blazsolar/climbr-cli"
   url "https://github.com/blazsolar/climbr-cli/releases/download/v0.1/climbr-0.1-darwind_m1.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "<SHA256>"
+  sha256 "057e5c3418e277a75770fa00a4fe8c9f193276ee24c66da7d1227752b11b3f65"
   head "https://github.com/blazsolar/climbr-cli.git"
 
   def install
