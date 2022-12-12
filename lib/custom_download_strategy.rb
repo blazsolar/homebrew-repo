cb9dcce066704d5557c71b232d8ccbaf9e3f350d
Blaz Solar <me@blaz.solar>
2022-12-12 13:37:47 +0100

    change to exec
    Signed-off-by: Blaz Solar <me@blaz.solar>

--- lib/custom_download_strategy.rb
+++ lib/custom_download_strategy.rb
@@ -69,7 +69,7 @@ class GitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
   private
 
   def _fetch(url:, resolved_url:, timeout:)
-      system("dh api download_url > temporary_path")
+      exec "dh api download_url > temporary_path"
     #curl_download download_url, to: temporary_path
   end
 
@@ -118,7 +118,7 @@ class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDo
 
   def _fetch(url:, resolved_url:, timeout:)
       ohai "gh release download -R #{@owner}/#{@repo} #{@tag} -p #{@filename} -O #{@temporary_path}"
-      system("gh release download -R #{@owner}/#{@repo} #{@tag} -p #{@filename} -O #{@temporary_path}")
+      system "gh release download -R #{@owner}/#{@repo} #{@tag} -p #{@filename} -O #{@temporary_path}"
     # HTTP request header `Accept: application/octet-stream` is required.
     # Without this, the GitHub API will respond with metadata, not binary.
     #curl_download download_url, "--header", "Accept: application/octet-stream", "--header", "Authorization: token #{@github_token}", to: temporary_path
