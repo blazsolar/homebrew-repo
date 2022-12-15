00ae7c0dcd9fb2ba887c1ea430986388127252f5
Blaz Solar <me@blaz.solar>
2022-12-15 15:38:07 +0100

    Fix

--- lib/custom_download_strategy.rb
+++ lib/custom_download_strategy.rb
@@ -26,7 +26,7 @@ class GitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
             #"/usr/bin/curl",
             #"/opt/homebrew/bin/gh"
         ].compact.map { |c|
-            ohai "Test #{c}
+        ohai "Test #{c}"
             Pathname(c)
         }.find(&:executable?)
         raise "No executable `gh` was found" unless @gh
